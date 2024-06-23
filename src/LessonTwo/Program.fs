6e37ba3542e0b54fa6adeaf2b2aa5d904dc8c859
Markus <markusreynolds1989@gmail.com>
2024-06-23 08:45:19 -0500

    Lesson 2.2

--- src/LessonTwo/Program.fs
+++ src/LessonTwo/Program.fs
@@ -12,6 +12,10 @@ x <- 4
 // The question then, is how do we do anything useful if we are worried about side effects?
 // We will cover that in depth later, but first let's understand the concept of pure functions.
 
+// Note, that you can shadow variables which means you can reassign them. I don't recommend this because it can cause
+// confusion on what the state of the variable is.
+// let y = 344
+
 // In .NET this is called a doc tag. This enables IDEs to link robust documentation about functions and methods to
 // the functions so when they are called you can get more information about them. Just hover over the function name
 // and see the description.
@@ -21,8 +25,7 @@ x <- 4
 /// </summary>
 /// <param name="inputOne"></param>
 /// <param name="inputTwo"></param>
-let pureFunction inputOne inputTwo =
-    inputOne + inputTwo
+let pureFunction inputOne inputTwo = inputOne + inputTwo
 
 // Functions like these allow us to optimize our code and write it in cleaner ways than if we were to use mutation
 // and side effects. For instance, if you always knew that certain inputs would generate certain outputs you
@@ -32,7 +35,45 @@ let pureFunction inputOne inputTwo =
 
 // Exercise 2.1
 // Before moving on to the next sub-lesson create a function that takes the height and width of a rectangle
-// and returns its area. 
+// and returns its area.
 // Remember you can experiment in F# Interactive to get instant feedback.
 // Here's a starter signature:
 // let area height width = ??
+
+// Lesson 2.2
+// Let's explore higher-order functions. We went over them a little bit in 1.6, but now we can explore what they are
+// about and what superpowers they unlock.
+
+// We talked before about a function that can take another function as an argument.
+let slottedFunction f argument = f argument
+
+// Observe the two functions below, we are passing a different function into each slottedFunction.
+// Observe also the syntax for what we call "anonymous functions" these are functions that we can
+// declare inline and don't need to be named. The let name binding above is a syntactic shortcut for this.
+let result = (fun x -> x + 3) 3
+let resultTwo = (fun y -> y + 3.0) 3.4
+// Last observation, observe that we can use different types and different type functions to pass into slottedFunction.
+// F# is a strongly typed language, this is a powerful tool called generics we will talk more about later.
+// Remember that we can also return a function we create from another function.
+let createdFunction argument = fun () -> argument
+
+// Now that we understand a higher order function and passing functions, let's talk about some higher order functions
+// that are built into F#. We always want to reach for the tools that are built in instead of making our own if
+// we can help it.
+// Let's look at a few of the most common ones.
+let groceries = [ "bananas"; "milk"; "chips"; "eggs" ]
+
+// Suppose we want to make all of these capital and return a new list with those changes.
+// In an imperative and mutable context we might just loop through each element of the list and alter them.
+// But in F# and in functional programming, we are going to use a built-in function called 'map' that will
+// create a new list instead.
+
+// This will take every string in the list and make them all capitalized. However, notice the .ToUpper()
+// syntax we used. This is called a "method call". This is different from a function because this functionality
+// is attached to the variable itself. 
+let upperCaseGroceries =
+    List.map (fun (grocery: string) -> grocery.ToUpper()) groceries
+
+// Map is a function that is included in the List module. Map is available for most collections and takes every item
+// and does some function on them and then collects the result. We are "mapping" an item to another item.
+
