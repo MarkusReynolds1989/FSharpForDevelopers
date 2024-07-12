f3d2cd9feb9fe0903f5133f45183531a0468d75d
markus <markusreynolds1989@gmail.com>
2024-07-12 06:16:35 -0500

    Test modifications.

--- src/Tests/Tests.fs
+++ src/Tests/Tests.fs
@@ -1,7 +1,17 @@
 module Tests
 
+open System
+open System.Net
+open System.Net.Http
+open EHRServer
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Testing
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
 open Xunit
+open Giraffe
 
 [<Fact>]
 let ``My test`` () =
-    Assert.True(true)
+    Assert.Equal(1, 1)
