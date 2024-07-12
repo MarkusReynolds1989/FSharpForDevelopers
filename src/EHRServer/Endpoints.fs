3e86873bfff50d6ed3cd708b05daec6341617518
markus <markusreynolds1989@gmail.com>
2024-07-11 20:02:01 -0500

    Updating endpoints.

--- src/EHRServer/Endpoints.fs
+++ src/EHRServer/Endpoints.fs
@@ -4,8 +4,23 @@ open Giraffe
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Logging
 
+type Patient =
+    { FirstName: string
+      LastName: string
+      Age: int
+      Height: double
+      Weight: double }
+
+let testPatients =
+    [| { FirstName = "Ted"
+         LastName = "Tedson"
+         Age = 23
+         Height = 1.8
+         Weight = 100 } |]
+
 let getPatients (logger: ILogger) =
-    fun (next: HttpFunc) (ctx: HttpContext) -> task {
-        logger.LogInformation("Handling GET /patients request") 
-        return! text "none" next ctx
-    }
+    fun (next: HttpFunc) (ctx: HttpContext) ->
+        task {
+            logger.LogInformation("Handling GET /patients request")
+            return! json testPatients next ctx
+        }
